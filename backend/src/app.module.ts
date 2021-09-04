8af4efeff696ab3cf02bf2949ccc86a236da19f3
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-09-04 13:15:26 +0200

    feat: add .env files for dev env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,30 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './profile-page-posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `../.env.stage.${process.env.STAGE}`,
+      validationSchema: configValidationSchema,
+    }),
     PostsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '!!okejhokej',
-      database: 'art-gallery',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+        };
+      },
     }),
     AuthModule,
   ],
