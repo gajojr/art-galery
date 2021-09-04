eae3fccc269afb347c143aa2beb1c960c38be5b6
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-09-04 10:48:45 +0200

    feat: add helmet and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const PORT = process.env.PORT || 5000;
